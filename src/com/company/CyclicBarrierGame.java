68a2dfc40c0c59e87c6e16f29b1261e0965875c4
ilgarbabayev <babayev.ilqar@gmail.com>
2020-11-21 18:15:54 +0200

    single Thread

--- src/com/company/CyclicBarrierGame.java
+++ src/com/company/CyclicBarrierGame.java
@@ -1,4 +1,17 @@
 package com.company;
 
+import java.util.concurrent.*;
+
 public class CyclicBarrierGame {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        ScheduledExecutorService service = Executors.newSingleThreadScheduledExecutor();
+
+        ScheduledFuture<?> result = service.schedule(() -> System.out.println("Runnable"), 5000, TimeUnit.MILLISECONDS);
+        Future<?> res = service.schedule(() -> "Callable", 5000, TimeUnit.MILLISECONDS);
+
+        System.out.println("R result: " + result.getDelay(TimeUnit.SECONDS));
+        System.out.println("C result: " + res.get());
+
+        service.shutdown();
+    }
 }
